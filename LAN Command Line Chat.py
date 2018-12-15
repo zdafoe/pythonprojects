0ce52b8e7024166be394e77b7dc6de5d5d1b5723
zdafoe <45864430+zdafoe@users.noreply.github.com>
2018-12-15 09:17:42 -0800

    Added more documentation

--- LAN Command Line Chat.py
+++ LAN Command Line Chat.py
@@ -4,6 +4,8 @@ from threading import Thread
 import time
 import sys
 import os
+
+
 # This function returns the host ip adress
 def get_ip():
     s = socket(AF_INET, SOCK_DGRAM)
@@ -15,6 +17,8 @@ def get_ip():
     finally:
         s.close()
     return IP
+
+
 #this function listens for messages sent to the host ip
 def serve(r):
     PORT_NUMBER = 5024
@@ -26,6 +30,8 @@ def serve(r):
         (data,addr) = mySocket.recvfrom(SIZE)
         if str(data).split(':')!='local IM at':
             print(str(data).replace("b'",'').replace("'",''))
+
+
 # This function broadcasts a message for other other machines running the script on the same local network to add the host ip to their send list
 def ping(r):
     hostip=get_ip()
@@ -39,6 +45,8 @@ def ping(r):
             hostlist[3]=str(x)
             broadcastsocket.sendto(broadcastdata,('.'.join(hostlist),5023))
         time.sleep(2)
+
+
 # This function listens for the message sent by other machines via the ping function and adds the other machines ips to the send list
 def getping(iplist,r):
     hostip=get_ip()
@@ -53,75 +61,76 @@ def getping(iplist,r):
             if sentip!=hostip:
                     if not sentip in sendaddresslist:
                         sendaddresslist.append(sentip)
+                        print(sentip,' Has joined the chat!')
                         iplist.put(sendaddresslist)
         except:
             pass
-# this process notifies the user if others have joined the groupchat and writes the send list to a temporary file to be used in sending texts in the groupchat
-def listupdate(iplist,r):
-    l2=[]
-    while r.running:
-        l1=iplist.get()
-        if l2!=l1:
-            for x in range(len(l2),len(l1)):
-                print(l1[x],' Has joined the chat!')
-            l2=l1
-            f=open('LANCHATIPS.temp','w')
-            f.write(','.join(l1))
-            f.close()
-        time.sleep(.11)
+
+
 #this class allows for the closing of all threads
 class run():
     def __init__(self):
         self.running=True
     def stop(self):
         self.running=False
+
+
 if __name__ == "__main__":
     hostip=get_ip()
+    # create a Queue to store the list of ip adresses
     iplist=Queue()
+
     r=run()
+
+    #create threads
     serverprocess=Thread(target=serve,args=(r,))
     pingprocess=Thread(target=ping,args=(r,))
     getpingprocess=Thread(target=getping,args=(iplist,r))
-    lsprocess=Thread(target=listupdate,args=(iplist,r))
-    serverprocess.start()
+
+
+    #start ping and ping recieving threads
     pingprocess.start()
     getpingprocess.start()
     sendsocket=socket(AF_INET, SOCK_DGRAM)
-    sendaddresslist=[]
+
+    # user instructions
     print('use: "-quit" to exit the chatroom')
     name=input('Enter your name: ')
-    gg.start()
+
+
+    #start thread to listen for and print messages from other local machines
+    serverprocess.start()
+    ipsendlist=[]
     while True:
         text=input()
+
+        # code for stopping all threads
         if text=='-quit':
             r.stop()
             broadcastsocket=socket(AF_INET, SOCK_DGRAM)
-            hostlist=hostip.split('.')
             broadcastsocket.sendto(b'',(hostip,5023))
             broadcastsocket.sendto(b'',(hostip,5024))
             iplist.put('')
             serverprocess.join()
             pingprocess.join()
             getpingprocess.join()
-            lsprocess.join()
             try:
                 os.remove("LANCHATIPS.temp")
             except:
                 pass
             break
+        #creates the final string to send to the list of ip's
         text='['+str(hostip)+'] '+name+': '+text
-        try:
-            f=open('LANCHATIPS.temp','r')
-            fl=f.read()
-            fl=fl.split(',')
-            if len(fl)>0:
-                send=True
-            else:
-                send=False
-        except:
-            fl=[]
+
+        #Checks for an updated list ip adresses
+        if not iplist.empty():
+            while not iplist.empty():
+                ipsendlist=iplist.get()
+            send=True
+        elif ipsendlist==[] :
             send=False
             print('there is no one else in the chatroom yet')
+        #sends message to all ip adresses on the list
         if send:
-            for address in fl:
+            for address in ipsendlist:
                     sendsocket.sendto(text.encode('utf-8'),(address,5024))
